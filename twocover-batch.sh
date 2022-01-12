376251690d7858d5301f9dc816b7a2c360eeef58
Daniel Hast <32797673+HastD@users.noreply.github.com>
2022-01-12 16:11:55 -0500

    Updated Magma version; removed Haswell CPU exclusion; adjusted runtime limits.

--- twocover-batch.sh
+++ twocover-batch.sh
@@ -9,13 +9,10 @@
 # Specify hard time limit for the job. 
 #   The job will be aborted if it runs longer than this time.
 #   The default time is 12 hours
-#$ -l h_rt=2:00:00
+#$ -l h_rt=3:00:00
 
 # Require avx instruction set
 #$ -l avx
-
-# Exclude Haswell architecture CPUs, which are having problems with Magma
-#$ -l cpu_arch=!haswell
 
 # Join stdout and stderr streams
 #$ -j y
@@ -27,7 +24,7 @@
 #$ -m ea
 
 module load sagemath/9.3
-module load magma_usyd/2.26-9
+module load magma_usyd/2.26-10
 
 # Use the SGE_TASK_ID environment variable to select the appropriate input file from bash array
 # Bash array index starts from 0, so we need to subtract one from SGE_TASK_ID value
