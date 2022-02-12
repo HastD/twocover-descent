144f21626b653bca1364c9a8cb12dd020fcc0b40
Daniel Hast <32797673+HastD@users.noreply.github.com>
2022-02-12 15:29:14 -0500

    Added qsub script to record Ecov map data

---
+++ Ecov-batch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+# Run using the following command:
+# qsub -t 1-77 Ecov-batch.sh
+
+# Set SCC project
+#$ -P arithgeo
+
+# Specify hard time limit for the job. 
+#   The job will be aborted if it runs longer than this time.
+#   The default time is 12 hours
+#$ -l h_rt=3:00:00
+
+# Require avx instruction set
+#$ -l avx
+
+# Run only on nodes with Magma license
+#$ -l magma
+
+# Join stdout and stderr streams
+#$ -j y
+
+# Specify location of stdout stream
+#$ -o /projectnb/arithgeo/drhast/twocover-results/stdout
+
+# Send an email when the job finishes or if it is aborted (by default no email is sent).
+#$ -m ea
+
+module load sagemath/9.3
+module load magma_usyd/2.26-10
+
+# Use the SGE_TASK_ID environment variable to select the appropriate input file from bash array
+# Bash array index starts from 0, so we need to subtract one from SGE_TASK_ID value
+
+for i in {0..99}
+do
+    index=$(( ($SGE_TASK_ID - 1)*100 + $i ))
+    if [ $index -lt 7692 ]
+    then
+        sage --python ./twocover-processor.sage.py --index $index --output_directory /projectnb/arithgeo/drhast/twocover-results --stages map
+    fi
+done
+
